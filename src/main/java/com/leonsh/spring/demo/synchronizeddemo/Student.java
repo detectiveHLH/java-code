c3177bf997a12b487bc93f8e42c4d44e16bfee7a
leonsh <detectivehlh@qq.com>
2022-03-09 12:32:16 +0800

    update
    Change-Id: I65eb3cf5356b4eed769289288cc699266295777c

---
+++ src/main/java/com/leonsh/spring/demo/synchronizeddemo/Student.java
@@ -0,0 +1,15 @@
+package com.leonsh.spring.demo.synchronizeddemo;
+
+import lombok.Data;
+
+/**
+ * Student
+ *
+ * @author leonsh
+ * @date 2022-03-03 10:21
+ **/
+@Data
+public class Student {
+    private String name;
+    private String email;
+}
