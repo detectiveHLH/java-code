8783140ef1f20a686f74fb0dd6f843cb924f1307
leonsh <detectivehlh@qq.com>
2022-05-25 11:06:41 +0800

    update
    Change-Id: Ic7cf5df3a3de0d728c059ba91c899acd8ccf480b

--- src/main/java/com/leonsh/java/code/JavaCodeApplication.java
+++ src/main/java/com/leonsh/java/code/JavaCodeApplication.java
@@ -3,9 +3,14 @@ package com.leonsh.java.code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * JavaCodeApplication
+ *
+ * @author hulunhao
+ */
 @SpringBootApplication
 public class JavaCodeApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(JavaCodeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodeApplication.class, args);
+    }
 }
